79d71cf918c4613492cbe68c33cc5e5536d4d559
bbb237 <kalea.sastra@gmail.com>
2025-10-31 19:36:26 -0400

    removed skeleton comments + added ps3_Comprehension_Q.pdf

--- ps3.rkt
+++ ps3.rkt
@@ -20,10 +20,6 @@
 ;;
 ;; For grading, the test cases all result in values that can be converted to base values.
 
-;(define (parse (s : s-expression)) : Expr
-;  (error 'parse "Not yet implemented.")
-;  )
-
 (define (parse (s : s-expression)) : Expr
   (cond
     [(s-exp-number? s) (numC (s-exp->number s))]
@@ -43,67 +39,27 @@
             [(lambda) (lambdaC (s-exp->symbol (second l)) (parse (third l)))]
             
             ;; ======== PAIRS ========
-            [(pair)
-             ;; (pair e1 e2)
-             (pairC (parse (second l)) (parse (third l)))]
-
-            [(fst)
-             ;; (fst e)
-             (fstC (parse (second l)))]
-
-            [(snd)
-             ;; (snd e)
-             (sndC (parse (second l)))]
+            [(pair) (pairC (parse (second l)) (parse (third l)))]
+            [(fst) (fstC (parse (second l)))]
+            [(snd) (sndC (parse (second l)))]
 
             ;; ======== BOXES ========
-            [(box)
-             (boxC (parse (second l)))]
-
-            [(unbox)
-             (unboxC (parse (second l)))]
-
-            [(set-box!)
-             (setboxC (parse (second l)) (parse (third l)))]
+            [(box) (boxC (parse (second l)))]
+            [(unbox) (unboxC (parse (second l)))]
+            [(set-box!) (setboxC (parse (second l)) (parse (third l)))]
 
             ;; ======== VECTORS ========
-            [(vector)
-             ;; (vector e1 e2 ... en)
-             (vectorC (map parse (rest l)))]
-
-            [(vector-ref)
-             ;; (vector-ref vecE idxE)
-             (vector-refC (parse (second l)) (parse (third l)))]
-
-            [(vector-set!)
-             ;; (vector-set! vecE idxE val-expr)
-             (vector-set!C (parse (second l)) (parse (third l)) (parse (fourth l)))]
-
-            [(vector-length)
-             ;; (vector-length vecE)
-             (vector-lengthC (parse (second l)))]
-
-            [(vector-make)
-             ;; (vector-make n-expr init-expr)
-             (vector-makeC (parse (second l)) (parse (third l)))]
-
-            [(subvector)
-             ;; (subvector vecE offset-expr len-expr)
-             (subvectorC (parse (second l)) (parse (third l)) (parse (fourth l)))]
+            [(vector) (vectorC (map parse (rest l)))]
+            [(vector-ref) (vector-refC (parse (second l)) (parse (third l)))]
+            [(vector-set!) (vector-set!C (parse (second l)) (parse (third l)) (parse (fourth l)))]
+            [(vector-length) (vector-lengthC (parse (second l)))]
+            [(vector-make) (vector-makeC (parse (second l)) (parse (third l)))]
+            [(subvector) (subvectorC (parse (second l)) (parse (third l)) (parse (fourth l)))]
 
             ;; ------------------------------------------------
-            [(begin)
-             ;; (begin e1 e2 ... en) -> (beginC (list (parse e1) ... (parse en))).
-             (beginC (map parse (rest l)))]
-
-            ;; ======== TRANSACTIONS ========
-            [(transact)
-             ;; (transact e)
-             (transactC (parse (second l)))]
-
-            ;; ------------------------------------------------
-            [else
-             ;; (e1 e2)
-             (appC (parse (first l)) (parse (second l)))]
+            [(begin) (beginC (map parse (rest l)))]
+            [(transact) (transactC (parse (second l)))]
+            [else (appC (parse (first l)) (parse (second l)))]
             )]
 
          [else
@@ -111,15 +67,9 @@
          ))]
     ))
 
-
-;; ============================================================
-;; Runtime values, environment, store, results
-;; ============================================================
-
-;; A Location will represent an index into the store.
 (define-type-alias Location number)
 
-;; Value = runtime values.
+;; used in eval-expr
 (define-type Value
   [numV (n : number)]
   [boolV (b : boolean)]
@@ -129,7 +79,6 @@
   [vectorV (loc : (listof Location)) (offset : number) (len : number)]
   )
 
-;; Environment mapping identifiers to Values.
 (define-type Binding
   [bind (name : symbol) (val : Value)])
 
@@ -155,11 +104,11 @@
 (define-type-alias Store (listof Cell))
 (define empty-store empty)
 
-;; fresh location (classic lecture6 trick: next index is (length sto))
+;; new location
 (define (alloc-location (sto : Store)) : Location
   (length sto))
 
-;; look up the Value at a given Location
+;; fetch the Value at a given Location
 (define (store-fetch (sto : Store) (loc : Location)) : Value
   (cond
     [(empty? sto)
@@ -180,22 +129,17 @@
          (cons (first sto)
                (store-update (rest sto) loc v)))]))
 
-;; allocate a new location to hold v, return loc + new store
 (define-type AllocResult
   [alloc-res (loc : Location) (store : Store)])
 
-;; Allocate a new cell in the store to hold v,
-;; returning the fresh location and the updated store.
+;; Allocate a new cell in the store to hold v, returns loc + updated store.
 (define (store-alloc (sto : Store) (v : Value)) : AllocResult
   (let ([newloc (alloc-location sto)])
     (alloc-res newloc (cons (cell newloc v) sto))))
 
-;; The result of evaluating an Expr is a Value plus updated Store.
 (define-type Res
   [res (v : Value) (s : Store)] )
 
-;; eval-expr : Expr Env Store -> Res
-;; returns a runtime Value and the new Store
 (define (eval-expr (e : Expr) (env : Env) (sto : Store)) : Res
   (type-case Expr e
     [numC (n) (res (numV n) sto)]
@@ -244,27 +188,18 @@
     [lambdaC (x body) (res (closV x body env) sto)]
 
     [appC (funE argE)
-          ;; (funE argE)
-          ;; TODO:
-          ;; 1. eval funE -> should be closV
-          ;; 2. eval argE
-          ;; 3. extend closure env with (param = argVal)
-          ;; 4. eval closure body in that extended env, threading store
           (let* ([rfun (eval-expr funE env sto)]
                  [vfun (res-v rfun)]
                  [sfun (res-s rfun)]
                  [rarg (eval-expr argE env sfun)]
                  [varg (res-v rarg)]
                  [sto-arg (res-s rarg)])
-            ;; TODO: ensure vfun is closV, otherwise error
             (let ([param (closV-arg vfun)]
                   [body  (closV-body vfun)]
                   [fenv  (closV-env vfun)])
               (eval-expr body (extend-env fenv param varg) sto-arg))
             )]
-
-    ;; --------------------------------------------------------
-    ;; pairs
+    
     [pairC (e1 e2)
            (let* ([r1 (eval-expr e1 env sto)]
                   [v1 (res-v r1)]
@@ -286,14 +221,8 @@
                  [sto1 (res-s r1)])
             (res (pairV-snd v1) sto1))]
 
-    ;; --------------------------------------------------------
-    ;; boxes
+    ;; ======== BOXES ========
     [boxC (e1)
-          ;; (box e1)
-          ;; TODO:
-          ;; 1. eval e1
-          ;; 2. allocate new location in store for that value
-          ;; 3. return boxV(loc)
           (let* ([r1 (eval-expr e1 env sto)]
                  [v1 (res-v r1)]
                  [sto1 (res-s r1)]
@@ -302,10 +231,6 @@
                  (alloc-res-store a)))]
 
     [unboxC (e1)
-            ;; (unbox e1)
-            ;; TODO:
-            ;; 1. eval e1 -> boxV loc
-            ;; 2. read from store at loc
             (let* ([r1 (eval-expr e1 env sto)]
                    [v1 (res-v r1)]
                    [sto1 (res-s r1)])
@@ -313,12 +238,6 @@
                 (res (store-fetch sto1 loc) sto1)))]
 
     [setboxC (e1 e2)
-             ;; (set-box! e1 e2)
-             ;; TODO:
-             ;; 1. eval e1 -> boxV loc
-             ;; 2. eval e2 -> newval
-             ;; 3. update store[loc] = newval
-             ;; 4. result is newval (or whatever spec says)
              (let* ([r1 (eval-expr e1 env sto)]
                     [v1 (res-v r1)]
                     [sto1 (res-s r1)]
@@ -329,8 +248,7 @@
                       [sto3  (store-update sto2 loc v2)])
                  (res v2 sto3)))]
 
-    ;; --------------------------------------------------------
-    ;; vectors
+    ;; ======== VECTORS ========
     [vectorC (elems)
              (cond
                [(empty? elems) (res (vectorV empty 0 0) sto)]
@@ -354,7 +272,6 @@
                )]
 
     [vector-refC (vecE idxE)
-                 ;; (vector-ref v i)
                  (let* ([rvec (eval-expr vecE env sto)]
                         [vvec (res-v rvec)]
                         [svec (res-s rvec)])
@@ -365,7 +282,6 @@
                                      [sidx (res-s ridx)])
                                 (type-case Value vidx
                                   [numV (n)
-                                        ;; find location of element (offset + n) in loc
                                         (let* ([target-index (+ offset n)]
                                                [target-loc (list-ref loc target-index)]
                                                [elem-val (store-fetch sidx target-loc)])
@@ -376,13 +292,6 @@
                    )]
 
     [vector-set!C (vecE idxE val-expr)
-                  ;; (vector-set! vec idx val)
-                  ;; TODO:
-                  ;; 1. eval vecE -> vectorV
-                  ;; 2. eval idxE -> numV
-                  ;; 3. eval val-expr
-                  ;; 4. mutate list-of-Values and build a new list with the index replaced.
-                  ;; 5. return (probably the value)
                   (let* ([rvec (eval-expr vecE env sto)]
                          [vvec (res-v rvec)]
                          [svec (res-s rvec)])
@@ -414,66 +323,21 @@
                                  (res (numV vlen) svec)]
                         [else
                          (error 'eval-expr "vector-length on non-vector")]))]
-
-;    [vector-makeC (len-expr init-expr)
-;                  (let* ([rlen  (eval-expr len-expr env sto)]
-;                         [vlen  (res-v rlen)]
-;                         [slen  (res-s rlen)])
-;                    (type-case Value vlen
-;                      [numV (n)
-;                            (let* ([rinit (eval-expr init-expr env slen)]
-;                                   [vinit (res-v rinit)]
-;                                   [sinit (res-s rinit)])
-;                              (letrec ([build
-;                                        (lambda (k cur-store acc-locs)
-;                                          (if (zero? k)
-;                                              (values acc-locs cur-store)
-;                                              (let* ([ar (store-alloc cur-store vinit)]
-;                                                     [new-loc    (alloc-res-loc ar)]
-;                                                     [new-store  (alloc-res-store ar)])
-;                                                (build (- k 1) new-store (cons new-loc acc-locs)))
-;                                              ))
-;                                        ])
-;                                (let-values ([(locs-rev final-store) (build n sinit empty)])
-;                                  (let ([locs-in-order (reverse locs-rev)])
-;                                    (res (vectorV locs-in-order 0 n) final-store))))
-;                              )]
-;                      [else (error 'eval-expr "vector-make length not a number")])
-;                    )]
     
     [vector-makeC (len-expr init-expr)
-                  ;; (vector-make n v)
-                  ;; 1. evaluate n -> must be a number
-                  ;; 2. evaluate v -> initial element value
-                  ;; 3. allocate n cells in the store, each holding that value
-                  ;; 4. build (vectorV loc 0 n), where loc is the list of those Locations
-                  ;; 5. return (res that-vector final-store)
                   (let* ([rlen  (eval-expr len-expr env sto)]
                          [vlen  (res-v rlen)]
                          [slen  (res-s rlen)])
                     (type-case Value vlen
                       [numV (n)
-                            ;; evaluate the initializer in the updated store from length
                             (let* ([rinit (eval-expr init-expr env slen)]
                                    [vinit (res-v rinit)]
                                    [sinit (res-s rinit)])
-                              ;; build : number Store (listof Location) -> Res
-                              ;; allocates k copies of vinit, threading the store.
-                              ;; acc-rev is the list of element locations we've allocated so far,
-                              ;; in reverse order (we'll reverse once at the end).
                               (letrec ([build
                                         (lambda (k cur-store acc-rev)
                                           (if (zero? k)
-                                              ;; done:
-                                              ;; reverse acc-rev to get locations in index order
-                                              ;; index 0 should correspond to the first allocated location
                                               (let ([loc-list (reverse acc-rev)])
-                                                ;; vectorV wants:
-                                                ;;   loc    : (listof Location) = loc-list
-                                                ;;   offset : number            = 0
-                                                ;;   len    : number            = n
                                                 (res (vectorV loc-list 0 n) cur-store))
-                                              ;; otherwise allocate one more cell for vinit
                                               (let* ([ar        (store-alloc cur-store vinit)]
                                                      [new-loc   (alloc-res-loc ar)]
                                                      [new-store (alloc-res-store ar)])
@@ -481,8 +345,6 @@
                                                        new-store
                                                        (cons new-loc acc-rev))))
                                           )])
-                                ;; kick off the allocator with k = n, starting store = sinit,
-                                ;; and an empty accumulator of locations
                                 (build n sinit empty))
                               )]
                       [else (error 'eval-expr "vector-make length not a number")])
@@ -511,8 +373,6 @@
                     [else (error 'eval-expr "subvector on non-vector")])
                   )]
 
-    ;; --------------------------------------------------------
-    ;; begin / sequencing
     [beginC (es)
             (cond
               [(empty? es)
@@ -527,17 +387,7 @@
                  (eval-expr (beginC (rest es)) env sto1))]
               )]
 
-    ;; --------------------------------------------------------
-    ;; transact
     [transactC (body)
-               ;; (transact body)
-               ;; TODO (spec from ps3):
-               ;; 1. snapshot original store
-               ;; 2. eval body using a copy
-               ;; 3. expect result to be (pair (bool commit?) result-val)
-               ;; 4. if commit? is #true => keep new store
-               ;;    if #false => discard new store, keep old
-               ;; 5. return result-val with whichever store survived
                (let ([start-store sto])
                  (let* ([rbody (eval-expr body env sto)]
                         [vbody (res-v rbody)]
@@ -547,8 +397,8 @@
                             (type-case Value firstv
                               [boolV (b)
                                      (if b
-                                         (res secondv sbody)       ;; commit
-                                         (res secondv start-store) ;; rollback
+                                         (res secondv sbody)      
+                                         (res secondv start-store)
                                          )]
                               [else
                                (error 'eval-expr "transact did not return (pair boolean value)")])]
@@ -564,22 +414,12 @@
     (letrec ([value->base
               (lambda (v)
                 (type-case Value v
-                  [numV (n)
-                        ;; TODO: number -> numBV
-                        (numBV n)]
-                  [boolV (b)
-                         ;; TODO: boolean -> boolBV
-                         (boolBV b)]
-                  [pairV (l r)
-                         ;; TODO: recursively convert pair sides
-                         (pairBV (value->base l)
-                                 (value->base r))]
-                  [closV (arg body env)
-                         (error 'eval-base "function values are not base")]
-                  [boxV (loc)
-                        (error 'eval-base "box values are not base")]
-                  [vectorV (loc offset len)
-                           (error 'eval-base "vector values are not base")]
+                  [numV (n) (numBV n)]
+                  [boolV (b) (boolBV b)]
+                  [pairV (l r) (pairBV (value->base l) (value->base r))]
+                  [closV (arg body env) (error 'eval-base "function values are not baseValues")]
+                  [boxV (loc) (error 'eval-base "box values are not baseValues")]
+                  [vectorV (loc offset len) (error 'eval-base "vector values are not baseValues")]
                   ))
               ])
       (value->base v)))
